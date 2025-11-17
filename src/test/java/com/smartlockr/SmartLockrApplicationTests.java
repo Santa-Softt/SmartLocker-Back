53cef302e653883a7faf5415b0ac661667e926c5
fr4ncisx <fr4ncisx@gmail.com>
2025-11-17 17:10:59 -0300

    Initial project build

---
+++ src/test/java/com/smartlockr/SmartLockrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlockr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLockrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
