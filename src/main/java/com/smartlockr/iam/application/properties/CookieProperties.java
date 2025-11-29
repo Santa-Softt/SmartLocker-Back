8a67ff431700921302ed7b67882819356d23c6c0
fr4ncisx <fr4ncisx@gmail.com>
2025-11-29 02:30:24 -0300

    feat(security): add security properties binding

---
+++ src/main/java/com/smartlockr/iam/application/properties/CookieProperties.java
@@ -0,0 +1,15 @@
+package com.smartlockr.iam.application.properties;
+
+import com.smartlockr.iam.domain.enums.SameSite;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "security.cookies")
+public record CookieProperties(
+        @NotNull boolean secure,
+        @NotNull SameSite sameSite
+) {
+}
+
