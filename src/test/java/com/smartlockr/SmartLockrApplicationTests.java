1bc4e66e4573cc981aa8bb44ea0049ffbcf08d8b
fr4ncisx <fr4ncisx@gmail.com>
2025-11-29 02:46:03 -0300

    chore: updated main file to prevent test this file

--- src/test/java/com/smartlockr/SmartLockrApplicationTests.java
+++ src/test/java/com/smartlockr/SmartLockrApplicationTests.java
@@ -1,13 +1,5 @@
 package com.smartlockr;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SmartLockrApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
