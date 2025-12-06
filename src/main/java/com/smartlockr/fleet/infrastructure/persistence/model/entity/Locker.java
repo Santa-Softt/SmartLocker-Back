0b41a62ca3607f6ebef5e899b531992b9721e3a9
fr4ncisx <fr4ncisx@gmail.com>
2025-12-06 14:31:31 -0300

    feat: add locker entity

---
+++ src/main/java/com/smartlockr/fleet/infrastructure/persistence/model/entity/Locker.java
@@ -0,0 +1,39 @@
+package com.smartlockr.fleet.infrastructure.persistence.model.entity;
+
+import com.smartlockr.fleet.domain.enums.LockerSize;
+import com.smartlockr.fleet.domain.enums.LockerState;
+import com.smartlockr.rental.infrastructure.persistence.entity.model.Rental;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(name = "lockers",
+        indexes = { @Index(name = "idx_locker_label", columnList = "label", unique = true) })
+public class Locker implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @UuidGenerator
+    @Id
+    private UUID uuid;
+    @Column(nullable = false, unique = true)
+    private String label;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LockerSize size;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LockerState state;
+    @OneToMany(mappedBy = "locker", fetch = FetchType.LAZY)
+    private List<Rental> rentals;
+}
