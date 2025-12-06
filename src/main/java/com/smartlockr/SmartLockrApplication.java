a3a0d96f52938ac8e15ec05b9da9fa88d8b37eac
fr4ncisx <fr4ncisx@gmail.com>
2025-12-06 14:30:04 -0300

    feat: add future pagination serialized dto

--- src/main/java/com/smartlockr/SmartLockrApplication.java
+++ src/main/java/com/smartlockr/SmartLockrApplication.java
@@ -3,7 +3,9 @@ package com.smartlockr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class SmartLockrApplication {
