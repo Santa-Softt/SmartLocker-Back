3352166bc5e1e31ad41b804310c710451e032c86
fr4ncisx <fr4ncisx@gmail.com>
2025-12-16 19:38:14 -0300

    feat: add graphql controller getting all available lockers by size

---
+++ src/main/java/com/smartlockr/fleet/infrastructure/graphql/query/LockerQueryResolver.java
@@ -0,0 +1,22 @@
+package com.smartlockr.fleet.infrastructure.graphql.query;
+
+import com.smartlockr.commons.annotations.GraphQLController;
+import com.smartlockr.fleet.infrastructure.graphql.dto.LockerResponse;
+import com.smartlockr.fleet.application.service.FleetService;
+import com.smartlockr.fleet.domain.enums.LockerSize;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+
+import java.util.List;
+
+@GraphQLController
+@RequiredArgsConstructor
+public class LockerQueryResolver {
+    private final FleetService fleetService;
+
+    @QueryMapping
+    public List<LockerResponse> getAvailableLockersBySize(@Argument LockerSize size) {
+        return fleetService.findAvailableLockersBySize(size);
+    }
+}
