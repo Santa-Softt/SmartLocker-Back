5b59689d889205ea74ac4a6f9c06cf99a2297a2d
fr4ncisx <fr4ncisx@gmail.com>
2025-12-16 19:39:15 -0300

    feat: add rental response

---
+++ src/main/java/com/smartlockr/rental/infrastructure/graphql/dto/RentalResponse.java
@@ -0,0 +1,16 @@
+package com.smartlockr.rental.infrastructure.graphql.dto;
+
+import com.smartlockr.fleet.infrastructure.graphql.dto.LockerResponse;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record RentalResponse(
+        UUID id,
+        LockerResponse locker,
+        Instant startTime,
+        Instant estimatedEndTime,
+        boolean isPenalized
+
+) {
+}
