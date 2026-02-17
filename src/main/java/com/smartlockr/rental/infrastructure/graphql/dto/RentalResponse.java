34f27dfaecaf0f4042de3004b77f368a52d81776
fr4ncisx <fr4ncisx@gmail.com>
2026-02-16 23:23:04 -0300

    Updated RentalResponse with Expiration time

--- src/main/java/com/smartlockr/rental/infrastructure/graphql/dto/RentalResponse.java
+++ src/main/java/com/smartlockr/rental/infrastructure/graphql/dto/RentalResponse.java
@@ -10,6 +10,7 @@ public record RentalResponse(
         LockerResponse locker,
         Instant startTime,
         Instant estimatedEndTime,
+        Instant holdExpiresAt,
         boolean isPenalized
 
 ) {
