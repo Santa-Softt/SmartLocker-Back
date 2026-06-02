0bd017fe00b97419217c919ffc61a75d6bd8f516
fr4ncisx <fr4ncisx@gmail.com>
2026-06-02 12:07:10 -0300

    chore(test): remove broken and duplicate test scaffolding
    - Delete empty SmartLockrApplicationTests placeholder
    - Delete unused ValidationException class in test sources
    - Rename LockerQueryResolverTestIT to LockerQueryResolverIT (Test suffix redundant)
    - Rename LockerSubscriptionResolverTest to LockerSubscriptionResolverIT

--- src/test/java/com/smartlockr/SmartLockrApplicationTests.java
+++
@@ -1,5 +0,0 @@
-package com.smartlockr;
-
-class SmartLockrApplicationTests {
-
-}
--- src/test/java/com/smartlockr/iam/application/exception/ValidationException.java
+++
@@ -1,6 +0,0 @@
-package com.smartlockr.iam.application.exception;
-
-import lombok.experimental.StandardException;
-
-@StandardException
-public class ValidationException extends RuntimeException {}
